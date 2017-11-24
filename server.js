6cd96d53d65330127caea0c32019bc85c9c1a13d
US\prituppalapati <prituppalapati@deloitte.com>
2017-11-25 04:14:01 +0530

    changes

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var socketIO = require('socket.io');
 var express = require('express');
 var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
 var compression = require('compression');
 
-var http = require('http').Server(app);
 
 app.use(compression());
 app.use(express.static(path.join(__dirname, './dist')));
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+
+
+/// app runs in port
 app.listen(process.env.PORT || 3000, function () {
     console.log('listening at 3000');
 });
-
