67e34f9a2495fbe33c7ace682161ed34b4a67cd4
US\prituppalapati <prituppalapati@deloitte.com>
2017-11-25 04:33:08 +0530

    changes

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var compression = require('compression');
 
-var socketIO = require('socket.io');
+var server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 
 
 app.use(compression());
@@ -14,12 +15,61 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-var server = require('http').createServer(app);
+server.listen(process.env.PORT || 3000);
 
-/// app runs in port
-server.listen(process.env.PORT || 3000, function () {
-    console.log('listening at 3000');
+
+io.sockets.on('connection', function (socket) {
+
+    // convenience function to log server messages on the client
+    function log() {
+        var array = ['Message from server:'];
+        array.push.apply(array, arguments);
+        socket.emit('log', array);
+    }
+
+    socket.on('message', function (message) {
+        log('Client said: ', message);
+        // for a real app, would be room-only (not broadcast)
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.on('create or join', function (room) {
+        log('Received request to create or join room ' + room);
+
+        var numClients = Object.keys(io.sockets.sockets).length;
+        log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+        if (numClients === 1) {
+            socket.join(room);
+            log('Client ID ' + socket.id + ' created room ' + room);
+            socket.emit('created', room, socket.id);
+
+        } else if (numClients === 2) {
+            log('Client ID ' + socket.id + ' joined room ' + room);
+            io.sockets.in(room).emit('join', room);
+            socket.join(room);
+            socket.emit('joined', room, socket.id);
+            io.sockets.in(room).emit('ready');
+        } else { // max two clients
+            socket.emit('full', room);
+        }
+    });
+
+    socket.on('ipaddr', function () {
+        var ifaces = os.networkInterfaces();
+        for (var dev in ifaces) {
+            ifaces[dev].forEach(function (details) {
+                if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+                    socket.emit('ipaddr', details.address);
+                }
+            });
+        }
+    });
+
+    socket.on('bye', function () {
+        console.log('received bye');
+    });
+
 });
-
 
 
