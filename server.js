40b1d9aeae2be412a3cc2dfc58c61d746f460561
US\prituppalapati <prituppalapati@deloitte.com>
2017-11-25 19:51:09 +0530

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-    .use(express.static(path.join(__dirname, './dist')))
+    .use(express.static(path.join(__dirname, './test')))
     .use((req, res) => res.sendFile(INDEX))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
